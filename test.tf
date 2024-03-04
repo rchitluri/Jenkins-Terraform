b4455957a955c751df810744f9c99c9949b75c82
rchitluri <160027160+rchitluri@users.noreply.github.com>
2024-03-04 06:12:22 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -17,7 +17,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-09988af04120b3591"
+   ami = "ami-0156b61643fdfee5c"
    instance_type = "t2.micro"
    key_name = "Practice_KP"
    tags = {
